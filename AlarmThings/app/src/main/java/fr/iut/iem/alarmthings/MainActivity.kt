efdd6db2f2a0b2bc2a2e808cd51ce4a74a833afd
louis-iut <32872097+louis-iut@users.noreply.github.com>
2018-04-30 17:12:53 +0200

    [FEAT] led + buzz on detect

--- AlarmThings/app/src/main/java/fr/iut/iem/alarmthings/MainActivity.kt
+++ AlarmThings/app/src/main/java/fr/iut/iem/alarmthings/MainActivity.kt
@@ -2,6 +2,10 @@ package fr.iut.iem.alarmthings
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.PeripheralManager
+import java.io.IOException
 
 /**
  * Skeleton of an Android Things activity.
@@ -23,10 +27,95 @@ import android.os.Bundle
  * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
  *
  */
+
 class MainActivity : Activity() {
+    companion object {
+        private val TAG = MainActivity.toString()
+        private const val GPIO_PIN_LED = "BCM2"
+        private const val GPIO_PIN_SENSOR = "BCM3"
+        private const val GPIO_PIN_BUZZER = "BCM4"
+    }
+
+    private lateinit var ledGpio: Gpio
+    private lateinit var sensorGpio: Gpio
+    private lateinit var buzzerGpio: Gpio
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpGpio()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        blinkLED()
+        detect()
+    }
+
+    private fun setUpGpio() {
+        val pioService = PeripheralManager.getInstance()
+        try {
+            Log.i(TAG, "Configuring GPIO pins")
+            ledGpio = pioService.openGpio(GPIO_PIN_LED)
+            ledGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+
+            sensorGpio = pioService.openGpio(GPIO_PIN_SENSOR)
+            sensorGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+
+            buzzerGpio = pioService.openGpio(GPIO_PIN_BUZZER)
+            buzzerGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+
+            Log.i(TAG, "Configuring successful")
+        } catch (e: IOException) {
+            Log.e(TAG, "Error configuring GPIO pins", e)
+        }
+    }
+
+    private fun buzz() {
+        val buzz = Runnable {
+            Log.i(TAG, "BUUUUUUUZZZZ")
+            buzzerGpio.value = true
+            sleep(50)
+            buzzerGpio.value = false
+            Log.i(TAG, "DONT BUZZ")
+        }
+        Thread(buzz).start()
+    }
+
+    private fun detect() {
+        val detector = Runnable {
+            while (true) {
+                if (sensorGpio.value) {
+                    Log.i(TAG, sensorGpio.value.toString())
+                    buzz()
+                }
+                sleep(100)
+            }
+        }
+
+        Thread(detector).start()
+    }
+
+    private fun blinkLED() {
+        val ledBlinker = Runnable {
+            while (true) {
+                // Turn on the LED
+                ledGpio.value = true
+                sleep(300)
+                // Turn off the LED
+                ledGpio.value = false
+                sleep(300)
+            }
+        }
+        Thread(ledBlinker).start()
+    }
+
+    private fun sleep(milliseconds: Int) {
+        try {
+            Thread.sleep(milliseconds.toLong())
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
     }
 }
